05f2d775ffdc1d29a3cb0a00b0a1c8f732814685
Sanechka <prostomenovich@gmail.com>
2024-07-07 21:23:41 +0300

    Set OpeGL context settings and callbacks to resize the window and close the window when pressing ESC

--- src/main.c
+++ src/main.c
@@ -1,21 +1,42 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+int windowSizeX = 1280;
+int windowSizeY = 720;
+
+void glfwWindowSizeCallback(GLFWwindow* window, int width, int height);
+void glfwKeyCallback(GLFWwindow* window, int key, int scancode, int action, int mode);
 
 int main(void)
 {
     GLFWwindow* window;
 
     /* Initialize the library */
-    if (!glfwInit())
-        return -1;
+    if (!glfwInit()){
+        printf("Error initializing GLFW!\n");
+        return EXIT_FAILURE;
+    }
+    
+    //Setting an OpenGL context
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    //Creating a Window
+    window = glfwCreateWindow(windowSizeX, windowSizeY, "BattleShip", NULL, NULL);
+
+    //Callbacks
+    glfwSetWindowSizeCallback(window, glfwWindowSizeCallback);
+    glfwSetKeyCallback(window, glfwKeyCallback);
+
+
     if (!window)
     {
+        printf("GLFW_CREATE_WINDOW_FAILED!\n");
         glfwTerminate();
-        return -1;
+        return EXIT_FAILURE;
     }
 
     /* Make the window's context current */
@@ -23,8 +44,12 @@ int main(void)
 
 	if (!gladLoadGL()){
 		printf("Can't load GLAD!\n");
-		return -1;
+        glfwTerminate();
+		return EXIT_FAILURE;
 	}
+
+    printf("Renderer: %s\n", glGetString(GL_RENDERER));
+    printf("OpenGL Version: %s\n", glGetString(GL_VERSION));
 
 	glClearColor(0, 1, 0, 1);
     /* Loop until the user closes the window */
@@ -41,5 +66,19 @@ int main(void)
     }
 
     glfwTerminate();
-    return 0;
+    return EXIT_SUCCESS;
+}
+
+void glfwWindowSizeCallback(GLFWwindow* window, int width, int height)
+{
+    windowSizeX = width;
+    windowSizeY = height;
+    glViewport(0, 0, windowSizeX, windowSizeY); 
+}
+
+void glfwKeyCallback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS){
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    }
 }
